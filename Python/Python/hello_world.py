67b899739407ebe4343f6d785e3fb4191d755742
Urian Chang <urianchang@gmail.com>
2017-02-06 11:23:06 -0800

    Tried another method of including variables in strings

--- Python/Python/hello_world.py
+++ Python/Python/hello_world.py
@@ -1,8 +1,10 @@
 def say_hello(name):
     if name:
-        print 'Hello, ' + name + ' from inside the function'
+        #print 'Hello, ' + name + ' from inside the function'
+        print "Hello, %s!" % (name)
     else:
         print 'No name'
+
 print 'Outside of the function'
 
 say_hello("urian")
