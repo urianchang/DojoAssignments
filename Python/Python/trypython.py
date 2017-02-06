ee27dcc8bd619ba151f2c33ac23211736c2dfc3a
Urian Chang <urianchang@gmail.com>
2017-02-06 11:20:26 -0800

    Added notes on MySQL. Started on Python Fundamentals with notes and the first couple of assignments.

---
+++ Python/Python/trypython.py
@@ -0,0 +1,13 @@
+# Create a variable
+first = "hello"
+print first
+# Reassign a new value to the same variable
+first = 5
+print first
+# Print a mathematical equation
+print 2+2
+# Add 2 strings together
+print "adios" + "amigo"
+# Add a string and integer. Tried two methods.
+print "adios" + "8"
+print "adios" + str(8)
