4cffaae27ef061cdb67ee7c225fdef8ad94e3489
Urian Chang <urianchang@gmail.com>
2017-02-06 21:18:25 -0800

    Completed Make Change assignment in Python. Added to Python notes.

---
+++ Python/Python/makechange.py
@@ -0,0 +1,34 @@
+"""
+Make Change
+
+Write a function that takes an amount of money in cents and returns the
+fewest number of coins possible for the number of cents. The output should be
+a dictionary.
+
+Assuming American currency:
+Dollar: 1
+Half-Dollar: 0.5 (optional)
+Quarter: 0.25
+Dime: 0.1
+Nickel: 0.05
+Penny: 0.01
+"""
+
+def coinage(cents):
+    coins = {}
+    if (cents >= 100):
+        coins["dollars"] = int(cents/100)
+        cents = cents%100
+    if (cents >= 50):
+        coins["half-dollars"] = 1
+        cents = cents - 50
+    coins["quarters"] = int(cents/25)
+    cents = cents%25
+    coins["dimes"] = int(cents/10)
+    cents = cents%10
+    coins["nickels"] = int(cents/5)
+    cents = cents%5
+    coins["pennies"] = cents
+    return coins
+
+print coinage(387)
