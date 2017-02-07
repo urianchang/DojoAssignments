f05f16ffec1b6d1f059f2b28946e02b5cd7c0295
Urian Chang <urianchang@gmail.com>
2017-02-06 22:46:57 -0800

    Changed Bubble Sort algorithm to use Boolean check so the loop stops running once swapping of values does not occur.

--- Python/Python/bubblesort.py
+++ Python/Python/bubblesort.py
@@ -6,14 +6,14 @@ Build an algorithm for bubble sort.
 
 def bubble(arr):
     size = len(arr)
-    count = 0
-    while (count < size):
-        count += 1
+    messy = True
+    while messy:
+        messy = False
         for num in range(0, size-1):
             if (arr[num] > arr[num+1]):
+                messy = True
                 arr[num], arr[num+1] = arr[num+1], arr[num]
-        print count
-        print arr
     return arr
 
 print bubble([6, 5, 3, 1, 8, 7, 2, 4])
+#print bubble([8, 7, 6, 5, 4, 3, 2, 1])
