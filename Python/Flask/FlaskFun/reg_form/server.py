a42e116ae238aa483e38b5c9ce6c3684d4086c37
Urian Chang <urianchang@gmail.com>
2017-02-08 21:52:33 -0800

    Added Birthday field and input validation. Added stricter password input validation.

--- Python/Flask/FlaskFun/reg_form/server.py
+++ Python/Flask/FlaskFun/reg_form/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, flash, request
 import re
+import time
 
 app = Flask(__name__)
 app.secret_key = '3'
@@ -17,8 +18,9 @@ def registerMe():
     mail = request.form['mail']
     pw = request.form['password']
     cpw = request.form['confirm-password']
-    userinfo = [fname, lname, mail, pw, cpw]
-    fields = ["First name", "Last name", "E-mail", "Password", "Confirm password"]
+    bday = request.form['birthday']
+    userinfo = [fname, lname, mail, pw, cpw, bday]
+    fields = ["First name", "Last name", "E-mail", "Password", "Confirm password", "Birthday"]
     print "***", userinfo, "***"
     while checkstatus is False:
         checkstatus = True
@@ -45,6 +47,16 @@ def registerMe():
                     if userinfo[3] != userinfo[4]:
                         checkstatus = False
                         flash("Password fields do not match!", 'error')
+                    if not re.search(r'(?=.*[A-Z])(?=.*\d)', userinfo[3]):
+                        checkstatus = False
+                        flash("Password must have 1 uppercase letter and 1 number.", 'error')
+            if ind == 5:
+                if userinfo[5] > time.strftime("%Y/%m/%d"):
+                    checkstatus = False
+                    flash("Invalid birthday! Need to be from the past.", 'error')
+                if not re.search(r'^[0-9][0-9][0-9][0-9][\-][0-9][0-9][\-][0-9][0-9]', userinfo[5]):
+                    checkstatus = False
+                    flash("Invalid birthday! Check for YYYY-MM-DD format.", 'error')
         if checkstatus is True:
             flash("Thanks for registering!", 'thanks')
         return redirect('/')
