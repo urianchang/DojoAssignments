6b979506d6c1bb5c9576332dc2d4d24644a7fafd
Urian Chang <urianchang@gmail.com>
2017-02-08 16:38:02 -0800

    Added form validation and sessions to Dojo Survey.

--- Python/Flask/FlaskFun/dojo_survey/server.py
+++ Python/Flask/FlaskFun/dojo_survey/server.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, session, redirect, flash
 app = Flask(__name__)
+app.secret_key = 'hush-hush'
 @app.route('/')
 def form():
     return render_template("index.html")
 
 @app.route('/results', methods=['POST'])
 def create_user():
-    print "Got Info"
-    username = request.form['username']
-    userlocation = request.form['userlocation']
-    userlanguage = request.form['userlanguage']
-    comment = request.form['commentbox']
-    return render_template("results.html", name=username, loc=userlocation, lang=userlanguage, comm=comment)
+    print "******Got POST Info******"
+    if len(request.form['commentbox']) > 120:
+        flash("Whoa, Betsy! No more than 120 characters in the comment box!")
+        return redirect('/')
+    elif len(request.form['username']) < 1 or len(request.form['commentbox']) < 1:
+        if len(request.form['username']) < 1 and len(request.form['commentbox']) < 1:
+            flash("Name and comment box cannot be empty!")
+        elif len(request.form['username']) < 1:
+            flash("Name cannot be empty!")
+        elif len(request.form['commentbox']) < 1:
+            flash("Comment box cannot be empty!")
+        return redirect('/')
+    else:
+        flash("Good to go!")
+        session['name'] = request.form['username']
+        session['loc'] = request.form['userlocation']
+        session['lang'] = request.form['userlanguage']
+        session['comm'] = request.form['commentbox']
+        return redirect('/results')
 
+@app.route('/results')
+def show_info():
+    return render_template("results.html")
 
 app.run(debug=True)
