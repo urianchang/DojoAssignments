dd815ccb7506b2db7573d280875b9deedca09d7c
Urian Chang <urianchang@gmail.com>
2017-02-10 11:09:19 -0800

    Added LEFT JOIN instead of JOIN. Also, included other methods shown in the answer key (for future reference).

--- Python/MySQL/sakila/myanswers.sql
+++ Python/MySQL/sakila/myanswers.sql
@@ -4,8 +4,8 @@ USE sakila;
 -- Your query should return customer first name, last name, email, and address.
 SELECT customer.first_name, customer.last_name, customer.email, address.address, city.city, address.city_id 
 FROM customer 
-JOIN address ON customer.customer_id = address.address_id
-JOIN city ON city.city_id = address.city_id
+LEFT JOIN address ON customer.customer_id = address.address_id
+LEFT JOIN city ON city.city_id = address.city_id
 WHERE address.city_id = 312;
 
 
@@ -18,13 +18,26 @@ JOIN film_category ON film.film_id = film_category.film_id
 JOIN category ON film_category.category_id = category.category_id
 WHERE film_category.category_id = 5;
 
+-- FROM THE ANSWER KEY --
+SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name as genre
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
+WHERE category.name = 'Comedy';
+
 
 -- 3. What query would you run to get all the films joined by actor_id=5? 
 -- Your query should return the film title, description, and release year.
 SELECT film_actor.actor_id, CONCAT(actor.first_name, " ", actor.last_name) AS actor, film.film_id, film.title, film.description, film.release_year
 FROM film
-JOIN film_actor ON film.film_id = film_actor.film_id
-JOIN actor ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film_actor ON film.film_id = film_actor.film_id
+LEFT JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film_actor.actor_id = 5;
+
+-- FROM ANSWER KEY --
+SELECT film.film_id, film.title, film.description, film.release_year
+FROM film 
+LEFT JOIN film_actor ON film.film_id = film_actor.film_id
 WHERE film_actor.actor_id = 5;
 
 
@@ -33,7 +46,7 @@ WHERE film_actor.actor_id = 5;
 -- Your query should return customer first name, last name, email, and address.
 SELECT customer.store_id, address.city_id, customer.first_name, customer.last_name, customer.email, address.address
 FROM customer
-JOIN address ON customer.address_id = address.address_id
+LEFT JOIN address ON customer.address_id = address.address_id
 WHERE customer.store_id = 1 AND (address.city_id = 1 OR address.city_id = 42 OR address.city_id = 312 OR address.city_id = 459);
 
 
@@ -42,8 +55,8 @@ WHERE customer.store_id = 1 AND (address.city_id = 1 OR address.city_id = 42 OR 
 -- Hint: You may use LIKE function in getting the 'behind the scenes' part.
 SELECT film.title, film.description, film.release_year, film.rating, film.special_features 
 FROM film
-JOIN film_actor ON film.film_id = film_actor.film_id
-JOIN actor ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film_actor ON film.film_id = film_actor.film_id
+LEFT JOIN actor ON actor.actor_id = film_actor.actor_id
 WHERE film.rating = "G" AND actor.actor_id = 15 AND film.special_features LIKE "%Behind the Scenes";
 
 
@@ -51,8 +64,8 @@ WHERE film.rating = "G" AND actor.actor_id = 15 AND film.special_features LIKE "
 -- Your query should return the film_id, title, actor_id, and actor_name.
 SELECT film.film_id, film.title, actor.actor_id, CONCAT(actor.first_name, " ", actor.last_name) AS actor
 FROM film
-JOIN film_actor ON film.film_id = film_actor.film_id
-JOIN actor ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film_actor ON film.film_id = film_actor.film_id
+LEFT JOIN actor ON actor.actor_id = film_actor.actor_id
 WHERE film.film_id = 369;
 
 
@@ -60,10 +73,18 @@ WHERE film.film_id = 369;
 -- Your query should return film title, description, release year, rating, special features, and genre (category).
 SELECT film.film_id, film.title, film.description, film.release_year, film.rating, film.special_features, category.name as genre, film.rental_rate
 FROM film
-JOIN film_category ON film.film_id = film_category.film_id
-JOIN category ON film_category.category_id = category.category_id
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+LEFT JOIN category ON film_category.category_id = category.category_id
 WHERE film.rental_rate = 2.99 AND category.name = "Drama";
 
+-- FROM ANSWER KEY (ANOTHER METHOD) --
+
+SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name as genre
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
+WHERE film.rental_rate = 2.99
+AND category.name = 'Drama';
 
 -- 8. What query would you run to get all the action films which are joined by SANDRA KILMER? 
 -- Your query should return film title, description, release year, rating, special features, genre (category), 
@@ -71,8 +92,19 @@ WHERE film.rental_rate = 2.99 AND category.name = "Drama";
 -- My notes: Sandra Kilmer actor id is 23
 SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name as genre, CONCAT(actor.first_name, " ", actor.last_name) as actor_name
 FROM film
-JOIN film_category ON film.film_id = film_category.film_id
-JOIN category ON film_category.category_id = category.category_id
-JOIN film_actor ON film.film_id = film_actor.film_id
-JOIN actor ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+LEFT JOIN category ON film_category.category_id = category.category_id
+LEFT JOIN film_actor ON film.film_id = film_actor.film_id
+LEFT JOIN actor ON actor.actor_id = film_actor.actor_id
 WHERE category.name = "Action" AND actor.actor_id = 23;
+
+-- FROM THE ANSWER KEY (ANOTHER METHOD) --
+SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name as genre, actor.first_name, actor.last_name
+FROM actor 
+LEFT JOIN film_actor ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film ON film.film_id = film_actor.film_id
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+LEFT JOIN category ON film_category.category_id = category.category_id
+WHERE actor.first_name = 'SANDRA'
+AND actor.last_name = 'KILMER'
+AND category.name = 'Action';
