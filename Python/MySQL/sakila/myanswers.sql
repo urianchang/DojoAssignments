7ff4cf160379c3b1eff4b2985474aad1da7eed45
Urian Chang <urianchang@gmail.com>
2017-02-09 22:59:01 -0800

    Sakila: 5/8 complete.

--- Python/MySQL/sakila/myanswers.sql
+++ Python/MySQL/sakila/myanswers.sql
@@ -40,7 +40,11 @@ WHERE customer.store_id = 1 AND (address.city_id = 1 OR address.city_id = 42 OR 
 -- 5. What query would you run to get all the films with a "rating = G" and "special feature = behind the scenes", 
 -- joined by actor_id = 15? Your query should return the film title, description, release year, rating, and special feature. 
 -- Hint: You may use LIKE function in getting the 'behind the scenes' part.
-
+SELECT film.title, film.description, film.release_year, film.rating, film.special_features 
+FROM film
+JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.rating = "G" AND actor.actor_id = 15 AND film.special_features LIKE "%Behind the Scenes";
 
 
 -- 6. What query would you run to get all the actors that joined in the film_id = 369? 
