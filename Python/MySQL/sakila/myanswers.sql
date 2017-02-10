67a9df1bf03c72cf194b9bcac7b3109f8f983dd9
Urian Chang <urianchang@gmail.com>
2017-02-09 23:02:38 -0800

    Sakila: 6/8 complete

--- Python/MySQL/sakila/myanswers.sql
+++ Python/MySQL/sakila/myanswers.sql
@@ -49,7 +49,11 @@ WHERE film.rating = "G" AND actor.actor_id = 15 AND film.special_features LIKE "
 
 -- 6. What query would you run to get all the actors that joined in the film_id = 369? 
 -- Your query should return the film_id, title, actor_id, and actor_name.
-
+SELECT film.film_id, film.title, actor.actor_id, CONCAT(actor.first_name, " ", actor.last_name) AS actor
+FROM film
+JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.film_id = 369;
 
 
 -- 7. What query would you run to get all drama films with a rental rate of 2.99? 
