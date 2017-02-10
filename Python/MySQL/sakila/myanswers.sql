b4d013228f4b3506e9a3e4762d3b5b7cde4dcf8f
Urian Chang <urianchang@gmail.com>
2017-02-09 22:47:20 -0800

    Working on Sakila: 4/8 complete

--- Python/MySQL/sakila/myanswers.sql
+++ Python/MySQL/sakila/myanswers.sql
@@ -31,6 +31,10 @@ WHERE film_actor.actor_id = 5;
 -- 4. What query would you run to get all the customers in store_id = 1 
 -- and inside these cities (1, 42, 312 and 459)? 
 -- Your query should return customer first name, last name, email, and address.
+SELECT customer.store_id, address.city_id, customer.first_name, customer.last_name, customer.email, address.address
+FROM customer
+JOIN address ON customer.address_id = address.address_id
+WHERE customer.store_id = 1 AND (address.city_id = 1 OR address.city_id = 42 OR address.city_id = 312 OR address.city_id = 459);
 
 
 -- 5. What query would you run to get all the films with a "rating = G" and "special feature = behind the scenes", 
