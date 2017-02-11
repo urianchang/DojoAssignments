e4fef9997bf61944447b068419161ef37f80311c
Urian Chang <urianchang@gmail.com>
2017-02-11 15:20:11 -0800

    Added practice exercise with how to interact with database.

---
+++ Python/flask_mysql/friends/server.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app,'friendsdb')
+
+@app.route('/')
+def index():
+    # Query database for friends
+    # define query
+    query = "SELECT * FROM friends"
+    # run query with query_db
+    friends = mysql.query_db(query)
+    # pass data to template
+    return render_template('index.html', all_friends=friends)
+
+# Inserting data into queries
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    # Write query to select specific user by id. At every point where
+    # we want to insert data, we write ":" and variable name.
+    query = "SELECT * FROM friends WHERE id = :specific_id"
+    # Then define a dictionary with key that matches :variable_name in query.
+    data = {'specific_id': friend_id}
+    # Run query with inserted data.
+    friends = mysql.query_db(query, data)
+    # Friends should be a list with a single object,
+    # so we pass the value at [0] to our template under alias one_friend.
+    return render_template('index.html', one_friend=friends[0])
+
+# Inserting Records
+@app.route('/friends', methods=['POST'])
+def create():
+    # check if we're getting all of the data from the form
+    # print request.form['first_name']
+    # print request.form['last_name']
+    # print request.form['occupation']
+
+    # add a friend to the database!
+    # Write query as a string. Notice how we have multiple values
+    # we want to insert into our query.
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+    # We'll then create a dictionary of data from the POST data received.
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'occupation': request.form['occupation']
+    }
+    # Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    return redirect('/')
+
+# Updating Records
+# Say we wanted to update a specific record, we could create another page and add a form that would submit to the following route.
+@app.route('/update_friend/<friend_id>', methods=['POST'])
+def update(friend_id):
+    query = "UPDATE friends
+             SET first_name = :first_name, last_name = :last_name, occupation = :occupation
+             WHERE id = :id"
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation'],
+             'id': friend_id
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+
+# Deleting Records
+@app.route('/remove_friend/<friend_id>', methods=['POST'])
+def delete(friend_id):
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    return redirect('/')
+
+app.run(debug=True)
