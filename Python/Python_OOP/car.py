37b3ba99000c3602e49fbc243f922d4cda74d422
Urian Chang <urianchang@gmail.com>
2017-02-14 15:24:09 -0800

    Completed the Car assignment.

--- Python/Python_OOP/car.py
+++ Python/Python_OOP/car.py
@@ -13,4 +13,15 @@ class Car(object):
             tax = 0.15
         self.tax = tax
     def display_all(self):
-        print 
+        print "\nPrice: $" + str(self.price)
+        print "Speed:", self.speed
+        print "Fuel:", self.fuel
+        print "Mileage:", self.mileage
+        print "Tax:", self.tax
+
+Car(2000, "35mph", "Full", "15mpg").display_all()
+Car(2000, "5mph", "Not Full", "105mpg").display_all()
+Car(2000, "15mph", "Kind of Full", "95mpg").display_all()
+Car(2000, "25mph", "Full", "25mpg").display_all()
+Car(2000, "45mph", "Empty", "25mpg").display_all()
+Car(2000000, "35mph", "Empty", "15mpg").display_all()
