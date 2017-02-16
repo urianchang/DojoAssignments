3fce28bb08ceb646d6cb3fc4b0b4cd441cc3c393
Urian Chang <urianchang@gmail.com>
2017-02-15 17:52:48 -0800

    Completed the Survey Form assignment.

--- Python/Django/survey_form/apps/survey/urls.py
+++ Python/Django/survey_form/apps/survey/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^results$', views.showResults),
-    url(r'^new_person$', views.new_user)
+    url(r'^new_person$', views.new_user),
+    url(r'^home$', views.goHome)
 ]
--- Python/Django/survey_form/apps/survey/views.py
+++ Python/Django/survey_form/apps/survey/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+    if 'cur_run' not in request.session:
+        request.session['cur_run'] = 0
     return render(request, 'survey/index.html')
 
 def showResults(request):
@@ -9,4 +11,12 @@ def showResults(request):
 
 def new_user(request):
     print "**** SENDING POST INFO ****"
+    request.session['uname'] = request.POST['username']
+    request.session['loc'] = request.POST['userlocation']
+    request.session['lang'] = request.POST['userlanguage']
+    request.session['comm'] = request.POST['commentbox']
+    request.session['cur_run'] += 1
     return redirect('/results')
+
+def goHome(request):
+    return redirect('/')
