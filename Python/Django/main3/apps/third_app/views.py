e8a1d129b174160ef8f36b4215c36c74ce8344cb
Urian Chang <urianchang@gmail.com>
2017-02-17 12:21:30 -0800

    Added to Django notes. Added lesson example about models in Django.

---
+++ Python/Django/main3/apps/third_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import People
+
+# Create your views here.
+def index(request):
+    People.objects.create(first_name="Mike", last_name="Hannon")
+    people = People.objects.all()
+    print people
+    return render(request, "third_app/index.html")
