3593f3ee423d520fe1a13e26587918f586f61282
Urian Chang <urianchang@gmail.com>
2017-02-21 12:52:30 -0800

    Started working on Sports ORM II

--- Python/Django/sports_orm/apps/leagues/views.py
+++ Python/Django/sports_orm/apps/leagues/views.py
@@ -127,11 +127,44 @@ def index(request):
 		"players": Player.objects.filter(last_name__icontains="Cooper").exclude(first_name__icontains="Joshua")
 	}
 	return render(request, "leagues/index.html", context)
-"""
+
 #16 ...all players with first name "Alexander" OR first name "Wyatt"
 def index(request):
 	context = {
 		"players": Player.objects.filter(first_name__icontains="Alexander")|Player.objects.filter(first_name__icontains="Wyatt")
+	}
+	return render(request, "leagues/index.html", context)
+"""
+"""
+SPORTS ORM II:
+1. ...all teams in the Atlantic Soccer Conference
+2. ...all (current) players on the Boston Penguins
+3. ...all (current) players in the International Collegiate Baseball Conference
+4. ...all (current) players in the American Conference of Amateur Football with last name "Lopez"
+5. ...all football players
+6. ...all teams with a (current) player named "Sophia"
+7. ...all leagues with a (current) player named "Sophia"
+8. ...everyone with the last name "Flores" who DOESN'T (currently) play for the Washington Roughriders
+9. ...all teams, past and present, that Samuel Evans has played with
+10. ...all players, past and present, with the Manitoba Tiger-Cats
+11. ...all players who were formerly (but aren't currently) with the Wichita Vikings
+12. ...every team that Jacob Gray played for before he joined the Oregon Colts
+13. ...everyone named "Joshua" who has ever played in the Atlantic Federation of Amateur Baseball Players
+14. ...all teams that have had 12 or more players, past and present. (HINT: Look up the Django annotate function.)
+15. ...all players, sorted by the number of teams they've played for
+"""
+"""
+#1 ...all teams in the Atlantic Soccer Conference
+def index(request):
+	context = {
+		"teams": Team.objects.filter(league=League.objects.filter(name__icontains="Atlantic", sport__icontains="soccer")),
+	}
+	return render(request, "leagues/index.html", context)
+"""
+#2 ...all (current) players on the Boston Penguins
+def index(request):
+	context = {
+		"players": Player.objects.filter(curr_team=Team.objects.filter(team_name__icontains="penguins", location__icontains="Boston")),
 	}
 	return render(request, "leagues/index.html", context)
 
