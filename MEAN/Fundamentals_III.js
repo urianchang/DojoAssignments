aacd1a1524238f84d8ebd4894529dc14627f19a2
Urian Chang <urianchang@gmail.com>
2017-04-03 13:08:06 -0700

    Started working on Fundamentals III assignment

---
+++ MEAN/Fundamentals_III.js
@@ -0,0 +1,6 @@
+/*
+JS Fundamentals III:
+
+Now create a ninjaConstructor that can be used to create Ninjas that each has a name, cohort, and belt-level.
+Give all of the Ninjas a “levelUp” method that increases their belt-level (Have all ninjas start at a yellow-belt).
+*/
