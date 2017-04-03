0e8039588c8a614013177b77d5ed89a6bbcb49df
Urian Chang <urianchang@gmail.com>
2017-04-03 01:21:33 -0700

    Added sample problem to reinforce hoisting concept

---
+++ MEAN/hoistingReminder.js
@@ -0,0 +1,8 @@
+// What happens if you do the following?
+console.log(a);
+var a = "weird";
+// Will it throw an error?  What will it print, if it doesn't throw an error?
+// How about this?
+console.log(typeof(b));
+var b = "weird too";
+console.log(typeof(b));
