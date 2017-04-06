27d760af7091fa98157c3ea6887cb5c937bc7ff6
Urian Chang <urianchang@gmail.com>
2017-04-05 19:28:07 -0700

    Completed Math Module assignment

---
+++ MEAN/NodeJS/math_module/app.js
@@ -0,0 +1,6 @@
+var mathlib = require('./mathlib')();
+//console.log(mathlib);
+console.log(mathlib.add(2, 3));
+console.log(mathlib.multiply(3, 5));
+console.log(mathlib.square(5));
+console.log(mathlib.random(1, 35));
