290c8b776efc2cffd25cafb75727d8992289f7ad
Urian Chang <urianchang@gmail.com>
2017-04-11 23:03:41 -0700

    Added to modularization notes. Created sample modularization app from learning platform

---
+++ MEAN/MongoDB/quoting_dojo_MOD/server/config/routes.js
@@ -0,0 +1,13 @@
+// here we load the Quote model that we created on the server.js page
+var quotes = require('../controllers/quotes.js');
+module.exports = function(app) {
+  app.get('/', function(req, res) {
+    res.render("index");
+  })
+  app.post('/quotes', function(req,res){
+    quotes.create(req, res);
+  })
+  app.get('/main', function (req, res) {
+    quotes.show(req, res);
+  })
+}
